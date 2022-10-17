748df77b76045755c9c3f7b4ebb3088c32bd7533
Grant <grant_d_lohman@aol.com>
2022-10-17 17:00:03 -0500

    Iss

---
+++ callbacks.js
@@ -0,0 +1,19 @@
+let animals = ['Giraffe', 'Elephant', 'Yak']
+
+animals.forEach( function(animal, index) {
+  console.log(animal, index)
+})
+
+animals.forEach( (animal, index) => {
+  console.log(animal, index)
+})
+
+animals.forEach( function(animal) {
+  console.log(animal)
+})
+
+animals.forEach( (animal) => {
+  console.log(animal)
+})
+
+animals.forEach( animal => console.log(animal) )
